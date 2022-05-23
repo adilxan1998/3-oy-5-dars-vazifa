50fcf2ed0b0ef224a0a2834dd21118a06b3b7039
adilxan1998 <odilxon.esonaliyev1998@gmail.com>
2022-05-23 08:21:39 +0500

    git init .

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const totalMoney = 9642192.3
+const money = prompt("summani kriting")
+
+if(money >= totalMoney){
+  console.log("Oq yo’l, Alisher!");
+}else if(money <= totalMoney) {
+  console.log("Alisher, ozgina sabr qilish kerak bo’lar ekan.");
+}
